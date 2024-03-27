cc84024164e5f751f5d63fac4fe4697db105738f
gabyc <gabriel.coquillet@gmail.com>
2024-03-27 18:22:32 +0100

    v1

---
+++ main.py
@@ -0,0 +1,88 @@
+# A --> 00
+# T --> 11
+# c --> 01
+# G --> 10
+
+class To_Dna:
+
+    def BinToDna(self,Bin):
+        Dna = ''
+        for i in range(0, len(Bin), 2):
+            if Bin[i] != '0' and Bin[i] != '1':
+                return False
+            if Bin[i + 1] != '0' and Bin[i + 1] != '1':
+                return False
+            if Bin[i] == '0':
+                if Bin[i + 1] == '1':
+                    Dna = Dna + 'C'
+                if Bin[i + 1] == '0':
+                    Dna = Dna + 'A'
+            if Bin[i] == '1':
+                if Bin[i + 1] == '1':
+                    Dna = Dna + 'T'
+                if Bin[i + 1] == '0':
+                    Dna = Dna + 'G'
+        return Dna
+
+    def TxtToBin(self,txt):
+        binaire = ''
+        binary = ''
+        for lettre in txt:
+            b = str(bin(ord(lettre)))
+            for i in range(2, len(b)):
+                binary = binary + b[i]
+            binaire = binaire + binary
+        for i in range(8):
+            if len(binaire) % 8 != 0:
+                binaire = '0' + binaire
+        return binaire
+
+    def TxtToDna(self,txt):
+        Dna = ''
+        bin = 0
+        for elt in txt:
+            bin = self.TxtToBin(elt)
+            Dna = Dna + self.BinToDna(bin)
+        return Dna
+
+
+class To_Txt:
+
+    def DnaToTxt(self,Dna):
+        bin = self.DnaToBin(Dna)
+        txt = self.BinToTxt(bin)
+        return txt
+
+    def DnaToBin(self,Dna):
+        bin = ''
+        for i in range(len(Dna)):
+            if Dna[i] == 'A':
+                bin = bin + '00'
+            elif Dna[i] == 'T':
+                bin = bin + '11'
+            elif Dna[i] == 'C':
+                bin = bin + '01'
+            elif Dna[i] == 'G':
+                bin = bin + '10'
+        return bin
+
+    def BinToTxt(self,Bin):
+        txt = ''
+        cpt = 0
+        for i in range(0, len(Bin) // 8):
+            bin = '0b' + Bin[cpt:8 + cpt]
+            ord = int(bin, 2)
+            txt += chr(ord)
+            cpt += 8
+        return txt
+
+ToDna = To_Dna()
+ToTxt = To_Txt()
+
+txt = "Hello World. Today we will try to convert txt to dna"
+
+dna = ToDna.TxtToDna(txt)
+
+txt_fin = ToTxt.DnaToTxt(dna)
+
+print(txt_fin)
